70215682f225bd9b39b8a01b899b7209efb692a6
Rustam27102006 <117748577+Rustam27102006@users.noreply.github.com>
2026-05-30 23:06:05 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,8 +12,10 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
+            IO.println("new" + i);
         }
         System.out.println("Hello World mama");
         System.out.println("What is good ?");
+        System.out.println("I have smth new here");
     }
 }
